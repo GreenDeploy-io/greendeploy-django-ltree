8f558582ec7a81d064145c5e80b015c25564c28e
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-09-09 18:16:38 +0800

    🚧chore: Add more tests to increase coverage
    previously: 83%
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- tests/test_path_value.py
+++ tests/test_path_value.py
@@ -1,5 +1,7 @@
 
-from django_ltree.fields import PathValue
+import pytest
+
+from django_ltree.fields import PathValue, PathValueProxy
 
 
 def test_create():
@@ -15,6 +17,12 @@ def test_create():
         yield 'beer'
 
     assert str(PathValue(generator())) == '100.bottles.of.beer'
+
+
+def test_invalid_value_for_path():
+    with pytest.raises(ValueError) as exc_info:
+        PathValue(object())  # or any other invalid type that should raise the error
+    assert "Invalid value" in str(exc_info.value)
 
 
 def test_path_value_repr():
@@ -41,3 +49,30 @@ def test_path_value_repr_with_list():
 def test_path_value_str_with_list():
     pv = PathValue([1, 2, 3])
     assert str(pv) == "1.2.3"
+
+
+class MockModel:
+    def __init__(self):
+        self.some_field = None
+
+
+def test_path_value_proxy_get():
+    proxy = PathValueProxy("some_field")
+    mock_instance = MockModel()
+
+    # Test the `if instance is None: return self` line
+    assert proxy.__get__(None, MockModel) is proxy
+
+    # Test the `if value is None: return value` line
+    assert proxy.__get__(mock_instance, MockModel) is None
+
+def test_path_value_proxy_set():
+    proxy = PathValueProxy("some_field")
+    mock_instance = MockModel()
+
+    # Test the `if instance is None: return self` line
+    assert proxy.__set__(None, "some_value") is None  # This won't raise an exception
+
+    # Set a value to test normal functionality
+    proxy.__set__(mock_instance, "some_value")
+    assert mock_instance.some_field == "some_value"
